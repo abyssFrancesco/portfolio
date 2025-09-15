79671ca8f09ca7b783708fd46cabac9ca3b7f00d
abyssFrancesco <gragnaniellofrancescom@gmail.com>
2025-09-15 10:04:38 +0200

    starting home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,17 @@ function Home() {
     <>
       <div className="home-box B">
         <Navbar />
-        <div className="home IB"></div>
+        <div className="home IB">
+          <div className="main B">
+            <div className="grid-box">
+              <div className="profile-main">
+
+              </div>
+              <div className="profile-card top-card"></div>
+              <div className="profile-card bottom-card"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
