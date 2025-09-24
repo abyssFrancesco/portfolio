3034233bc057a85af363002945a2ccb9a2b1719f
abyssFrancesco <gragnaniellofrancescom@gmail.com>
2025-09-24 14:49:09 +0200

    cv dwnl

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,38 +1,43 @@
 import React from "react";
-/* note to self, check if the css is connected before troubleshooting */
 import "./Navbar.css";
 import lighticon from "../../assets/icons8-sun.svg";
 import pfp from "../../assets/pfp.png";
 
 function Navbar() {
+  const handleDownload = () => {
+    const fileName = "Francesco Maria Gragnaniello cv.pdf"; // si trova in /public
+    const href = `${import.meta.env.BASE_URL}${encodeURIComponent(fileName)}`; 
+    const a = document.createElement("a");
+    a.href = href;
+    a.download = "Francesco_Maria_Gragnaniello_CV.pdf"; // nome del file salvato
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+  };
+
   return (
     <>
       <div className="navbar-box B">
         <div className="pfp IB">
-            <img src={pfp} alt="" />
+          <img src={pfp} alt="" />
         </div>
+
         <div className="navbar IB">
           <div className="nav IB">
             <nav>
               <ul>
-                <li>
-                  <a href="#home">Home</a>
-                </li>
-                <li>
-                  <a href="#about">About</a>
-                </li>
-                <li>
-                  <a href="#projects">Projects</a>
-                </li>
-                <li>
-                  <a href="#contact">Contact</a>
-                </li>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
               </ul>
             </nav>
           </div>
+
           <div className="resume">
-            <button>Resume</button>
+            <button type="button" onClick={handleDownload}>Resume</button>
           </div>
+
           <div className="light-icon IB">
             <img src={lighticon} alt="" />
           </div>
@@ -41,4 +46,5 @@ function Navbar() {
     </>
   );
 }
+
 export default Navbar;
