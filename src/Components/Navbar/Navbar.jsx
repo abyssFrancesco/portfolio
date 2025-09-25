34c2361847178d7392bc5bc24cb26f065f2a7f76
abyssFrancesco <gragnaniellofrancescom@gmail.com>
2025-09-25 22:21:33 +0200

    responsive V3

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import pfp from "../../assets/pfp.png";
 function Navbar() {
   const handleDownload = () => {
     const fileName = "Francesco Maria Gragnaniello cv.pdf"; // si trova in /public
-    const href = `${import.meta.env.BASE_URL}${encodeURIComponent(fileName)}`; 
+    const href = `${import.meta.env.BASE_URL}${encodeURIComponent(fileName)}`;
     const a = document.createElement("a");
     a.href = href;
     a.download = "Francesco_Maria_Gragnaniello_CV.pdf"; // nome del file salvato
@@ -26,21 +26,29 @@ function Navbar() {
           <div className="nav IB">
             <nav>
               <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li>
+                  <a href="#home">Home</a>
+                </li>
+                <li>
+                  <a href="#about">About</a>
+                </li>
+                <li>
+                  <a href="#projects">Projects</a>
+                </li>
+                <li>
+                  <a href="#contact">Contact</a>
+                </li>
               </ul>
             </nav>
-          </div>
-
-          <div className="resume">
-            <button type="button" onClick={handleDownload}>Resume</button>
-          </div>
-
-          <div className="light-icon IB">
+          </div>{" "}
+{/*           <div className="light-icon IB">
             <img src={lighticon} alt="" />
-          </div>
+          </div> */}
+{/*           <div className="resume">
+            <button type="button" onClick={handleDownload}>
+              Resume
+            </button>
+          </div> */}
         </div>
       </div>
     </>
